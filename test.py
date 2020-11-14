e7765590ad2a25237b54e2bf986b1e1140e14a0f
Stuart Bonnell <stuartbonnell@hotmail.com>
2020-11-14 16:05:16 +0000

    Initial

---
+++ test.py
@@ -0,0 +1,15 @@
+import requests, json
+
+genresJson = requests.get("https://api.deezer.com/genre/")
+
+for genre in genresJson.json()['data']:
+  print (genre['id'], genre['name'])
+
+
+artistJson = requests.get("https://api.deezer.com/genre/95/artists")
+
+for artist in artistJson.json()['data']:
+  print (artist['id'], artist['name'])
+  
+
+
